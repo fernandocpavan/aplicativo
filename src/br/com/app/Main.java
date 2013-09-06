7296badd91a00c50a2805f1e79180c29bbbf5dbc
fernando_pavan <fernando_pavan@10.1.48.162>
2013-09-06 16:51:55 -0300

    Iniciando projeto

---
+++ src/br/com/app/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.app;
+
+import br.com.view.JanelaPrincipalGUI;
+
+/**
+ *
+ * @author fernando_pavan
+ */
+public class Main {
+    public static void main(String[] args) {
+       JanelaPrincipalGUI jp = new JanelaPrincipalGUI();
+       jp.setLocationRelativeTo(null);
+       jp.setVisible(true);
+    }
+    
+}
